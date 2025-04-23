686139bfe8066545ad7e631d5eaf961a56b20930
AMAN DHOMNE <107036303+Aman-Dhomne@users.noreply.github.com>
2025-04-24 04:39:49 +0530

    Add files via upload

---
+++ src/data/InputOpt.js
@@ -0,0 +1,22 @@
+export const Options = [
+    {
+        id: 1,
+        option: "Around on daily basis",
+    },
+    {
+        id:2,
+        option: "Not daily but frequently",
+    },
+    {
+        id: 3,
+        option: "Once or twice a week",
+    },
+    {
+        id: 4,
+        option: "Once or twice a month",
+    },
+    {
+        id: 5,
+        option: "Not at All",
+    },
+];
